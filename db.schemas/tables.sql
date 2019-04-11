6b1ad6ed4eeeaabc81689a46574918dcea0f7191
matenemeth-laptop <mate.nemeth@outlook.hu>
2019-04-11 20:00:48 +0200

    refactored db table schemas

--- db.schemas/tables.sql
+++ db.schemas/tables.sql
@@ -1,18 +1,19 @@
-CREATE TABLE carspec (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    make VARCHAR(50) NOT NULL,
-    model VARCHAR(50) NOT NULL,
-    engine INT NOT NULL,
-    age INT,
-    transmission VARCHAR(10),
-    city VARCHAR(20),
-    link VARCHAR(200)
-)
-
 CREATE TABLE carlist (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     platform VARCHAR(20) NOT NULL,
     platform_id INT NOT NULL,
     price INT NOT NULL,
     date_of_scan DATE DEFAULT NOW()
+)
+
+CREATE TABLE carspec (
+    id BIGINT NOT NULL PRIMARY KEY,   
+    make VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    engine INT NOT NULL,
+    fuel VARCHAR(8) NOT NULL,    
+    transmission VARCHAR(10),
+    link VARCHAR(200) NOT NULL,
+    city VARCHAR(20)
 )
