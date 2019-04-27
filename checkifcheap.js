be05e7d6abd43945d7a2cea342053caba0b71516
matenemeth-laptop <mate.nemeth@outlook.hu>
2019-04-27 09:29:25 +0200

    first try of mailing alerts to my own mail

--- checkifcheap.js
+++ checkifcheap.js
@@ -1,4 +1,5 @@
 const db = require('./db')
+const mailIt = require('./mailit')
 
 const checkIfCheap = async (id, type, price) => {
     const vehiclePriceStats = await db('average_prices').select().where('id', type).then(row => {
@@ -11,15 +12,12 @@ const checkIfCheap = async (id, type, price) => {
     if(vehiclePriceStats) {
         const avg = vehiclePriceStats.avg
         const median = vehiclePriceStats.median
-        if(price < avg * 0.75) {
+        if(price < avg * 0.75 || price < median * 0.75) {
             const link = await db('carlist').select().where('id', id).then(row => `${row[0].platform}${row[0].link}`)
-            const percentage = Math.round(price/avg * 100)
-            console.log(`olcsobb az atlagnal ${percentage}%-kal, itt: ${link}`)
-        }
-        if(price < median * 0.75) {
-            const link = await db('carlist').select().where('id', id).then(row => `${row[0].platform}${row[0].link}`)
-            const percentage = Math.round(price/median * 100)
-            console.log(`olcsobb a mediannal ${percentage}%-kal, itt: ${link}`)
+            const typeText = await db('cartype').select().where('id', type).then(row => `${row[0].make} ${row[0].model} (${row[0].age})`)
+            const avgPercent = Math.round(price/avg * 100)
+            const medianPercent = Math.round(price/median * 100)
+            return await mailIt(typeText, price, link, avgPercent, medianPercent)
         }
     }
 }
--- mailit.js
+++ mailit.js
@@ -3,19 +3,20 @@ const DOMAIN = 'mail.matenemeth.hu';
 const HOST = 'api.eu.mailgun.net'
 const mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN, host: HOST});
 
-const data = {
-  from: 'Car Alert <no-reply@mail.matenemeth.hu>',
-  to: 'mate.nemeth@outlook.hu',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomeness!'
-};
+const mailIt = async (typeText, price, link, avgPercent, medianPercent) => {
+  const data = {
+    from: 'Car Alert <no-reply@mail.matenemeth.hu>',
+    to: 'mate.nemeth@outlook.hu',
+    subject: `${typeText} - €${price},-`,
+    text: `A medián eladási ár ${medianPercent}%-a, az átlagár ${avgPercent}%-a.
+    A hírdetés itt található: ${link}`
+  };
+   
 
-mailgun.messages().send(data, (error, body) => {
-  console.log(body);
-});
 
-const mailIt = async () => {
-    return true
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body);
+  });
 }
 
 
