17828fdd5931581d91feb05b44c9383bb42e4c9c
matenemeth-laptop <mate.nemeth@outlook.hu>
2019-04-30 07:52:26 +0200

    email sending working with conditions stated in users json object

--- checkifneedsmailing.js
+++ checkifneedsmailing.js
@@ -7,11 +7,12 @@ const checkIfNeedsMailing = async (carSpec, typeId) => {
     if(await usersToAlert) {
         const link = await db('carlist').select().where('id', carSpec.id).then(row => `${row[0].platform}${row[0].link}`)
         const typeText = await db('cartype').select().where('id', typeId).then(row => `${row[0].make} ${row[0].model} (${row[0].age})`)
-        const avgPercent = Math.round(100-(price/avg * 100))
-        const medianPercent = Math.round(100-(price/median * 100))
+        const calculatedPrices = await db('average_prices').select().where('id', typeId).then(row=> row[0])
+        const avgPercent = Math.round(100-(carSpec.price/calculatedPrices.avg * 100))
+        const medianPercent = Math.round(100-(carSpec.price/calculatedPrices.median * 100))
         console.log('found a cheap car, mailing it!')
-        filteredUsers.map(user => {
-            mailIt(typeText, price, link, avgPercent, medianPercent, user)
+        usersToAlert.map(user => {
+            mailIt(typeText, carSpec.price, link, avgPercent, medianPercent, user)
         })
     }
 }
@@ -33,23 +34,29 @@ const filterUsers = async (carSpec, typeId) => {
             name: 'Nemeth Mate',
             email: 'mate.nemeth@outlook.hu',
             alerts: {
-                zipcodes: [10, 11, 12, 22, 24, 71, 40, 88],
-                treshold: 15
+                zipcodes: [],
+                treshold: 25
             }
         }
     ]
     
-    return await filterByPriceTreshold(carSpec, typeId, await filterByZip(carSpec.zipcode, users))
+    return await filterByPriceTreshold(carSpec, typeId, users)
 }
 
 const filterByZip = (zipcode, filteredUsers) => {
     const zip = Number(zipcode.toString().slice(0, 2))
     return filteredUsers.filter(user => {
-        return user.alerts.zipcodes.indexOf(zip) !== -1
+        console.log(user)
+        if(user.alerts.zipcodes.length === 0) {
+            return user
+        } else {
+            return user.alerts.zipcodes.indexOf(zip) !== -1
+        }
     })
 }
 
-const filterByPriceTreshold = async (carSpec, typeId, filteredUsers) => {
+const filterByPriceTreshold = async (carSpec, typeId, users) => {
+    const filteredUsers = await filterByZip(carSpec.zipcode, users)
     if (!filteredUsers) {
         return null
     } else {
@@ -62,9 +69,10 @@ const filterByPriceTreshold = async (carSpec, typeId, filteredUsers) => {
         })
         if(vehiclePriceStats) {
             const toAlert = filteredUsers.map(user => {
-                const treshold = (100 - user.alerts.treshold) / 100
+                const treshold = (100 - (user.alerts.treshold)) / 100
                 const { price } = carSpec
                 const { avg, median} = vehiclePriceStats
+                console.log(`treshold: ${treshold}, price: ${price}, avg alert at: ${avg * treshold}, median alert at: ${median * treshold}`)
                 if(price < (avg * treshold) || price < (median * treshold)) {
                     return user
                 } else {
