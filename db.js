5ad5f91e9e6093bb50b47d0a4f243d80634b26e1
matenemeth-DESKTOP <mate.nemeth@outlook.hu>
2020-05-02 18:19:49 +0200

    typo: porcess - process

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 const db = require('knex')({
   client: 'pg',
-  connection: `postgres://${process.env.POSTGRE_USER}:${porcess.env.POSTGRE_PASSWORD}@${process.env.POSTGRE_URL}`,
+  connection: `postgres://${process.env.POSTGRE_USER}:${process.env.POSTGRE_PASSWORD}@${process.env.POSTGRE_URL}`,
 });
 
 module.exports = db;
