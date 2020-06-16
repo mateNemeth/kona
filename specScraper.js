3c94b075af5fc5c96827269717f51556ed2c2c4e
mateNemeth <hello@matenemeth.hu>
2020-06-16 22:38:06 +0200

    fix fuel

--- specScraper.js
+++ specScraper.js
@@ -109,7 +109,9 @@ const carProcess = async (data, id) => {
           lookFor($('dt'), 'Üzemanyag').next().text().trim() ===
             'Super E10 Plus 95-ös / Super 95 (Particulate Filter)' ||
           lookFor($('dt'), 'Üzemanyag').next().text().trim() ===
-            '91-es normálbenzin / Super 95 / Super Plus 98-as / E10-es 91-es normálbenzin / Super E10 Plus 95-ös / Super Plus E10 98-as'
+            '91-es normálbenzin / Super 95 / Super Plus 98-as / E10-es 91-es normálbenzin / Super E10 Plus 95-ös / Super Plus E10 98-as' ||
+          lookFor($('dt'), 'Üzemanyag').next().text().trim() ===
+            'Super E10 Plus 95-ös / 91-es normálbenzin / E10-es 91-es normálbenzin / Super 95 / Super Plus 98-as / Super Plus E10 98-as'
         ) {
           return 'Benzin';
         } else {
